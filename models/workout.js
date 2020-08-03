3ae6a81b6f9a49e1a07fab0fad9fa6ea83d5668d
Paul Aglipay <paglipay@gmail.com>
2020-08-03 13:01:38 -0700

    virtuals mongodb

--- models/workout.js
+++ models/workout.js
@@ -31,7 +31,41 @@ const WorkoutSchema = new Schema({
         type: Number
       }
     }]
-});
+},
+  {
+    toJSON: {
+      virtuals: true
+    }
+  });
+  // adds a dynamically-created property to schema
+  WorkoutSchema.virtual("totalDuration").get(function () {
+    // "reduce" array of exercises down to just the sum of their durations
+    return this.exercises.reduce(function (total, exercise) {
+      return total + exercise.duration;
+    }, 0);
+  });
+  // adds a dynamically-created property to schema
+  WorkoutSchema.virtual("totalWeight").get(function () {
+    // "reduce" array of exercises down to just the sum of their durations
+    return this.exercises.reduce(function (total, exercise) {
+      return total + exercise.weight;
+    }, 0);
+  });
+  // adds a dynamically-created property to schema
+  WorkoutSchema.virtual("totalSets").get(function () {
+    // "reduce" array of exercises down to just the sum of their durations
+    return this.exercises.reduce(function (total, exercise) {
+      return total + exercise.sets;
+    }, 0);
+  });
+  // adds a dynamically-created property to schema
+  WorkoutSchema.virtual("totalReps").get(function () {
+    // "reduce" array of exercises down to just the sum of their durations
+    return this.exercises.reduce(function (total, exercise) {
+      return total + exercise.reps;
+    }, 0);
+  });
+
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
 
